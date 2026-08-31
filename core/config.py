290f32967d3533558f4d1b92c1b18dcd5e12e736
ntagiantas-dev <mitsosdg66@gmail.com>
2026-08-31 14:25:56 +0300

    Add __init__.py to 'core/'

---
+++ core/config.py
@@ -0,0 +1,18 @@
+# config.py
+import os
+
+# Το βασικό URL του BSD Football API
+BASE_URL = "https://sports.bzzoiro.com/api/v2"
+
+# Διαβάζει το API key αυτόματα από τις Environment Variables του Render
+API_KEY = os.getenv("BSD_API_KEY", "MISSING_API_KEY")
+
+# Οι 5 ελληνικές ομάδες που στοχεύουμε στο v0.1
+TARGET_COUNTRY = "GR"
+TARGET_TEAMS = [
+    "Olympiacos",
+    "AEK Athens",
+    "PAOK",
+    "Panathinaikos",
+    "Aris Thessaloniki"
+]
