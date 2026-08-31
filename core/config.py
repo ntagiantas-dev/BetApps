1c26ee930683ee8c295658b465e93b626540c4be
ntagiantas-dev <mitsosdg66@gmail.com>
2026-08-31 16:52:28 +0300

    Change Base_Url

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@
 import os
 
 # Το βασικό URL του BSD Football API (με το /football/ στη μέση)
-BASE_URL = "https://sports.bzzoiro.com/football/api/v2"
+BASE_URL = "https://sports.bzzoiro.com/api/v2"
 
 # Διαβάζει το API key αυτόματα από τις Environment Variables του Render
 API_KEY = os.getenv("BSD_API_KEY", "MISSING_API_KEY")
